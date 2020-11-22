20d1cc669916b72e33004af22b1d7ea3a8303976
Chathura Prabasha <chathuraprabasha@gmail.com>
2020-11-22 12:38:28 +0530

    Add to git

---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: QuoteList(),
+  ));
+}
+
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+  //List of quotes
+  List<Quote> quotes = [
+    Quote(
+        text:
+            "Your time is limited, so don't waste it living someone else's life.",
+        author: "author 1"),
+    Quote(
+        text:
+            "Always do your best. What you plant now, you will harvest later.",
+        author: "author 2"),
+    Quote(
+        text: "Action is the foundational key to all success.",
+        author: "author 2"),
+  ];
+
+  //quote Card constructor called into the main
+  Widget quoteTemplate(quote) {
+    return QuoteCard(
+      quote: quote,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Awsome Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote) => QuoteCard(quote: quote)).toList(),
+      ),
+    );
+  }
+}
+
+//Quote Card class defines how quote card is designed
+class QuoteCard extends StatelessWidget {
+  final Quote quote;
+
+  //quote card constructor with name parameters
+  QuoteCard({this.quote});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              quote.text,
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.grey[600],
+              ),
+            ),
+            SizedBox(height: 6.0),
+            Text(
+              quote.author,
+              style: TextStyle(
+                fontSize: 14.0,
+                color: Colors.grey[800],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
